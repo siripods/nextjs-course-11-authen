e9c86441710890a20ea0c04ea1d30d60d29a6054
siripods <74808448+siripods@users.noreply.github.com>
2023-09-23 23:45:37 +0700

    Handle session in getServerSideProps

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -4,28 +4,25 @@ import classes from './user-profile.module.css';
 import { getSession } from 'next-auth/react';
 
 function UserProfile() {
-  const [ isLoading, setIsLoading ] = useState(true);
-  //const { loadedSession, setLoadedSession } = useState();
-  useEffect(() => {
-    getSession().then(session => {
-      //setLoadedSession(session);
-      if (!session) {
-        // Redirect away if NOT auth
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-    })
-  }, [])
-  //const { data: session, status } = useSession();
-  //console.log("status = ", status);
-  //console.log("session = ", session);
+  // const [ isLoading, setIsLoading ] = useState(true);
+  // //const { loadedSession, setLoadedSession } = useState();
+  // useEffect(() => {
+  //   getSession().then(session => {
+  //     //setLoadedSession(session);
+  //     if (!session) {
+  //       // Redirect away if NOT auth
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   })
+  // }, [])
+ 
+  // console.log("isLoading = ", isLoading);
 
-  console.log("isLoading = ", isLoading);
-
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
   
 
   return (
